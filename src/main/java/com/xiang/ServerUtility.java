56af062a5082a7cbae4e87479a9a90fd7ba2a763
xiangCaO233 <odokoei@outlook.com>
2023-07-18 06:50:19 +0800

    initialize project again

--- src/main/java/com/xiang/ServerUtility.java
+++ src/main/java/com/xiang/ServerUtility.java
@@ -5,14 +5,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author xiang2333
- */
 public class ServerUtility implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("server-utility");
 
 	@Override
 	public void onInitialize() {
